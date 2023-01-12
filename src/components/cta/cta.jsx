ccac98f20a33ed1085c4947aea08b9b0dbbed5cf
success ibekwe <Successibekwe047@gmail.com>
2023-01-12 15:31:43 +0100

    created cta section

--- src/components/cta/cta.jsx
+++ src/components/cta/cta.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./cta.css";
 
 const cta = () => {
-  return <div>cta</div>;
+  return (
+    <div className="gpt3__cta">
+      <div className="gtp3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & stat exploring the endless possibilities</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default cta;
